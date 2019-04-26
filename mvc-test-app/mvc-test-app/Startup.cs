714c6e717852880950d321182e618735ea8d83c9
Pritesh Tayade <pritesht04@gmail.com>
2019-04-26 21:37:04 +0530

    intial commit

---
+++ mvc-test-app/mvc-test-app/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc_test_app.Startup))]
+namespace mvc_test_app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
